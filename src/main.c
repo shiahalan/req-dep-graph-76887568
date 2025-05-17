6502ddd98004a003393c760df0bcd57fec2bc9c0
Alan Shiah <102575877+shiahalan@users.noreply.github.com>
2025-05-17 16:32:14 -0700

    Added modules

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "../include/txtReader.h"
+#include "../include/graphParser.h"
+#include "../include/reportParser.h"
+
 
 int main(){
-
-	printf("Hello, World!");
+	printf("Generating SRS-Dependency-Graph.txt");
+	// If-else, if failed say something and exit
+	//
+	printf ("Generating SRS-Report.txt");
+	// If-else, if failed say somethign and exit
+	readfile("SRS-Dependency-Graph.txt");
+	readfile("SRS-Report.txt");
 }
